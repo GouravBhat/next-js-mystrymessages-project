15e63312b6d38593ad79aebb7f436354926f7850
gourav bhatt <gourav5@gmail.com>
2025-11-16 12:34:30 +0530

    added proxy.js middleware fix

--- src/proxy.js
+++ src/proxy.js
@@ -1,28 +1,32 @@
-import { NextResponse, NextRequest } from 'next/server'
-import {proxy} from 'next/server'
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-import { getToken } from "next-auth/jwt"
-// This function can be marked `async` if using `await` inside
-export default proxy(async (req) => {
+export default async function proxy(req) {
+  const token = await getToken({
+    req,
+    secret: process.env.AUTH_SECRET
+  });
 
+  const url = req.nextUrl;
 
-    const token=await getToken({req:request,secret: process.env.AUTH_SECRET})
-    
-    
-    
-    const url=req.nextUrl
-    
-    if(token && (url.pathname.startsWith("/sign-in")||url.pathname.startsWith("/signup")||url.pathname.startsWith("/verify")) ){
-        return NextResponse.redirect(new URL('/dashboard', req.url))
-    }
-    if(!token && url.pathname.startsWith("/dashboard") ){
-         return NextResponse.redirect(new URL('/signup', req.url))
-    }
+  // If user is logged in → prevent visiting auth pages
+  if (
+    token &&
+    (url.pathname.startsWith("/sign-in") ||
+      url.pathname.startsWith("/signup") ||
+      url.pathname.startsWith("/verify"))
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
 
-    return req;
-  
-})
- 
-export const config = {
-  matcher: ['/sign-in','/dashboard','/verify/:path*'],
+  // If user is NOT logged in → block dashboard
+  if (!token && url.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/signup", req.url));
+  }
+
+  return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/sign-in", "/signup", "/dashboard/:path*", "/verify/:path*"],
+};
